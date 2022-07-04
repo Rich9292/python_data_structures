db10770802aa3260ddde3533d3648cc746493eb9
rfields9 <rfields9@gatech.edu>
2022-07-04 21:33:51 +0200

    setup folder structure and created node class

---
+++ data_struct/test/node_test.py
@@ -0,0 +1,6 @@
+from ..src import node as nd
+
+
+def tester():
+    a = nd.node(5)
+    print(a)
---
+++ main.py
@@ -0,0 +1,8 @@
+import data_struct.src.node as nd
+import data_struct.test.node_test as nd_test
+
+
+
+# a = nd.node(4)
+
+nd_test.tester()
