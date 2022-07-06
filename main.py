242eb19b12df550b61152caea7f54458e56ad08d
rfields9 <rfields9@gatech.edu>
2022-07-06 19:30:03 +0200

    updated main function

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+# # can use this file to run data structures without installing package
+# # for example: 
 # import data_struct.src.node as nd
-import pytest
-import data_struct.test.node_test as nd_test
-
-
-# test node
-nd_test.tester()
+# some_node = nd.node(1)
+# print(some_node.get_value())
